496c11283d68acd41b4dc60789ccfec12b284fd6
Jorge Solis Pauwels <jorgeluissolis@gmail.com>
2014-04-04 11:53:20 +0200

    first commit

---
+++ ClarolineNetThemeBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Claroline\NetThemeBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+
+/**
+ * Bundle class.
+ */
+class ClarolineNetThemeBundle extends PluginBundle
+{
+    public function hasMigrations()
+    {
+        return false;
+    }
+}
